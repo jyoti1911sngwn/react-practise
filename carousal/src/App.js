83fd0301ea49fdd4b0ee5084cc0017a65d42703d
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2026-01-04 11:13:32 +0530

    infinite scroll

--- carousal/src/App.js
+++ carousal/src/App.js
@@ -1,31 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const Images = [
-    "/carosal1.jpg",
-    "/carosal2.jpg",
-    "/carosal3.jpg",
-  ]
-  const [currentIdx, setcurrentIdx] = useState(0)
-  const handlePrev=()=>{
-    setcurrentIdx((prev)=>
-    prev===0 ? Images.length - 1 : prev-1)
-  }
-  const handleNext=()=>{
-    setcurrentIdx((prev)=>
-    prev===Images.length-1 ? 0 : prev+1)
-  }
+const Image = [
+  'carosal1.jpg',
+  'carosal2.jpg',
+  'carosal3.jpg'
+]
+const[current, setCurrent ] = useState(0)
+const handlePrev=()=>{
+  setCurrent((prev)=>
+    prev === 0 ? Image.length-1 : prev-1
+  )
+}
+const handleNext=()=>{
+  setCurrent((prev)=>
+  prev ===Image.length-1 ? 0 : prev+1)
+}
+useEffect(()=>{
+  const interval = setInterval(()=>{
+    setCurrent((prev) =>  prev ===0 ? Image.length-1 : prev-1)
+  },300)
+  return ()=> clearInterval(interval)
+},[])
   return (
-    <div className="App" style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "20px" }}>
-      
-      <button style={{ padding: "10px 20px" }} onClick={handlePrev}>Previous</button>
-      
-      <img src={Images[currentIdx]} alt="carousel" style={{ width: "300px", height: "auto" }} />
-      
-      <button style={{ padding: "10px 20px" }} onClick={handleNext}>Next</button>
-      
-    </div>
+   <div className="App" style={{display: 'flex', flexDirection: 'row', alignItems:'center', justifyContent:'center', gap: '20px'}}>
+    <button style={{padding: '10px 20px'}} onClick={handlePrev}>Prev</button>
+    <img src={Image[current]} style={{height: '1000px'}}/>
+    <button style={{padding: '10px 20px'}} onClick={handleNext}>Next</button>
+   </div>
   );
 }
 
